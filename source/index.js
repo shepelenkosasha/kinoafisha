f17e2ed40e8879fb0c21ac2ad2a5b2a616987afd
Oleksandr Shepelenko <sashashepelenko@gmail.com>
2019-02-06 21:58:19 +0200

    add kinoafisha

--- source/index.js
+++ source/index.js
@@ -1,10 +1,106 @@
 // Core
-import React from 'react';
+import React, { memo, useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import { Spring } from 'react-spring';
 
 // Theme
 import './theme/init';
+import { getStyles } from './helpers';
+import { api } from './API';
 
-const start = <h1 className = 'welcome'>Добро пожаловать!</h1>;
+const Kinoafisha = memo(() => {
+    const [ selectedFilter, setSelectedFilter ] = useState('upcoming');
+    const [ movies, setMovies ] = useState([]);
 
-ReactDOM.render(start, document.getElementById('app'));
+    const _getMoviesByFilter = async (nextFilter) => {
+        const movies = await api.getMovies(nextFilter);
+
+        setMovies(movies);
+    };
+
+    const _updateMoviesByFilter = (event) => {
+        const nextFilter = event.currentTarget.dataset.name;
+
+        setSelectedFilter(nextFilter);
+        _getMoviesByFilter(nextFilter);
+    };
+
+    const styles = getStyles({
+        selectedFilter,
+    });
+
+    // componentDidMount + componentWillUnmount
+    useEffect(() => {
+        _getMoviesByFilter(selectedFilter);
+    }, []);
+
+    const moviesJSX = movies.map((movie) => {
+        return (
+            <Spring
+                from = {{
+                    opacity: 0,
+                }}
+                key = { movie.id }
+                to = {{
+                    opacity: 1,
+                }}>
+                {(props) => (
+                    <div
+                        className = 'movie'
+                        style = { props }>
+                        <div className = 'poster'>
+                            <span className = 'genre'>{movie.genre}</span>
+                            <img src = { movie.poster } />
+                            <span className = 'rating'>{movie.rating}</span>
+                        </div>
+                        <span className = 'title'>{movie.title}</span>
+                    </div>
+                )}
+            </Spring>
+        );
+    });
+
+    return (
+        <>
+            <div className = 'header'>
+                <div className = 'logo' />
+                <div className = 'filters'>
+                    <div
+                        className = { styles.latestFilter }
+                        data-name = 'latest'
+                        onClick = { _updateMoviesByFilter }>
+                        <span>Новинки 2018</span>
+                    </div>
+                    <div
+                        className = { styles.upcomingFilter }
+                        data-name = 'upcoming'
+                        onClick = { _updateMoviesByFilter }>
+                        <span>Скоро в кинотеатрах</span>
+                    </div>
+                    <div
+                        className = { styles.popularFilter }
+                        data-name = 'popular'
+                        onClick = { _updateMoviesByFilter }>
+                        <span>В топ-чартах</span>
+                    </div>
+                </div>
+            </div>
+            <div className = 'sorting'>
+                <button className = 'desc'>по новизне</button>
+            </div>
+            <div className = 'content'>{moviesJSX}</div>
+            <footer className = 'footer'>
+                <a href = 'mailto:team@lectrum.io'>team@lectrum.io</a>
+                <span>
+                    2018 © Все права защищены. Разработано с любовью <a href = 'https://lectrum.io/intensive/react' target = '_blank'>в Лектруме</a>
+                </span>
+                <div className = 'social'>
+                    <a href = 'https://www.facebook.com/lectrum/' target = '_blank' className = 'facebook'></a>
+                    <a href = 'https://t.me/lectrum' target = '_blank' className = 'telegram'></a>
+                </div>
+            </footer>
+        </>
+    );
+});
+
+ReactDOM.render(<Kinoafisha dat />, document.getElementById('app'));
